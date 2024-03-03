9c42acb1d9d4578b9ebd0aba2b6009f3e9f33fde
haram22 <kimhr9889@handong.ac.kr>
2024-03-03 18:13:35 +0900

    [Fix] resolve optional binding

--- Sources/PARD_DesignSystem/UIColor.swift
+++ Sources/PARD_DesignSystem/UIColor.swift
@@ -1,27 +1,22 @@
 import UIKit
 
 public extension UIColor {
-    private convenience init?(colorNamed: String) {
-        self.init(named: colorNamed, in: .module, compatibleWith: nil)
-    }
-
     static let pard = Pard()
 
     final class Pard: NSObject {
         override init() { super.init() }
 
         // Primary
-        public let primaryBlue = UIColor(colorNamed: "primaryBlue")!
-        public let primaryGreen = UIColor(colorNamed: "primaryGreen")!
-        public let primaryPurple = UIColor(colorNamed: "primaryPurple")!
+        public var primaryBlue: UIColor { UIColor(named: "primaryBlue", in: .module, compatibleWith: nil) ?? UIColor.blue }
+        public var primaryGreen: UIColor { UIColor(named: "primaryGreen", in: .module, compatibleWith: nil) ?? UIColor.green }
+        public var primaryPurple: UIColor { UIColor(named: "primaryPurple", in: .module, compatibleWith: nil) ?? UIColor.purple }
 
-        // else
-        public let errorRed = UIColor(colorNamed: "errorRed")!
-        public let white100 = UIColor(colorNamed: "white100")!
-        public let gray10 = UIColor(colorNamed: "gray10")!
-        public let gray30 = UIColor(colorNamed: "gray30")!
-        public let blackCard = UIColor(colorNamed: "blackCard")!
-        public let blackBackground = UIColor(colorNamed: "blackBackground")!
-
+        // Else
+        public var errorRed: UIColor { UIColor(named: "errorRed", in: .module, compatibleWith: nil) ?? UIColor.red }
+        public var white100: UIColor { UIColor(named: "white100", in: .module, compatibleWith: nil) ?? UIColor.white }
+        public var gray10: UIColor { UIColor(named: "gray10", in: .module, compatibleWith: nil) ?? UIColor.gray }
+        public var gray30: UIColor { UIColor(named: "gray30", in: .module, compatibleWith: nil) ?? UIColor.darkGray }
+        public var blackCard: UIColor { UIColor(named: "blackCard", in: .module, compatibleWith: nil) ?? UIColor.black }
+        public var blackBackground: UIColor { UIColor(named: "blackBackground", in: .module, compatibleWith: nil) ?? UIColor.black }
     }
 }

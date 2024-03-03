7eb3c12d9bd5c9cc9c685ab5f6aa895e9aa0a2f8
haram22 <kimhr9889@handong.ac.kr>
2024-03-03 18:48:19 +0900

    [Feat] create normal button

---
+++ Sources/PARD_DesignSystem/NormalButton.swift
@@ -0,0 +1,62 @@
+//
+//  File.swift
+//  
+//
+//  Created by 김하람 on 3/3/24.
+//
+
+import UIKit
+
+public final class NormalButton: UIView {
+    private let button = UIButton()
+    private var didTapHandler: (() -> Void)?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    public var isEnabled: Bool = true {
+        didSet {
+            button.isEnabled = isEnabled
+            button.backgroundColor = isEnabled ? .pard.primaryBlue : .pard.gray30.withAlphaComponent(0.2)
+        }
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        button.layer.cornerRadius = 4
+    }
+
+    public convenience init(
+        title: String,
+        didTapHandler: @escaping () -> Void
+    ) {
+        self.init(frame: .zero)
+        self.isUserInteractionEnabled = true
+        self.addSubview(button)
+        button.setTitle(title, for: .normal)
+        button.setTitle(title, for: .disabled)
+        button.setTitleColor(.pard.white100, for: .normal)
+        button.setTitleColor(.pard.white100, for: .disabled)
+        button.backgroundColor = .pard.gray30
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: self.topAnchor),
+            button.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        ])
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        self.didTapHandler = didTapHandler
+    }
+
+    @objc
+    private func didTapButton() {
+        didTapHandler?()
+    }
+}

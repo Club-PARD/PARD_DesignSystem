a0d66f671c1451faede20877de68eeab87130cf6
haram22 <kimhr9889@handong.ac.kr>
2024-03-03 16:35:51 +0900

    [init] add colors, except gradation color

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PARD_DesignSystem"
-//            resources: [.process("Resources")]
+            name: "PARD_DesignSystem",
+            resources: [.process("Resources")]
         ),
         
         .testTarget(
---
+++ Sources/PARD_DesignSystem/UIColor.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+public extension UIColor {
+    private convenience init?(colorNamed: String) {
+        self.init(named: colorNamed, in: .module, compatibleWith: nil)
+    }
+
+    static let pomodoro = Pomodoro()
+
+    final class Pomodoro: NSObject {
+        override init() { super.init() }
+        
+        public let background = UIColor(colorNamed: "background")!
+
+        // Primary
+        public let primaryBlue = UIColor(colorNamed: "primaryBlue")!
+        public let primaryGreen = UIColor(colorNamed: "primaryGreen")!
+        public let primaryPurple = UIColor(colorNamed: "primaryPurple")!
+
+        // else
+        public let errorRed = UIColor(colorNamed: "errorRed")!
+        public let white100 = UIColor(colorNamed: "white100")!
+        public let gray10 = UIColor(colorNamed: "gray10")!
+        public let gray30 = UIColor(colorNamed: "gray30")!
+        public let blackCard = UIColor(colorNamed: "blackCard")!
+        public let blackBackground = UIColor(colorNamed: "blackBackground")!
+
+    }
+}

7a8f6bcb462411dd4cf2131d4f2900e354a86038
haram22 <kimhr9889@handong.ac.kr>
2024-03-07 12:49:23 +0900

    [Fix] add textfield lay radius

--- Sources/PARD_DesignSystem/TextFieldComp.swift
+++ Sources/PARD_DesignSystem/TextFieldComp.swift
@@ -27,6 +27,7 @@ public final class TextFieldComp: UITextField {
     ) {
         self.init(frame: .zero)
         self.backgroundColor = .pard.blackCard
+        self.layer.cornerRadius = 4
         self.placeholder = placeHolder
         self.textColor = .white
         self.setPlaceholderColor(.pard.gray30)
@@ -36,7 +37,7 @@ public final class TextFieldComp: UITextField {
         self.leftView = leftPadding
         self.leftViewMode = UITextField.ViewMode.always
 
-        let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width + 10, height: self.self.frame.height))
+        let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width + 10, height: self.frame.height))
         self.rightView = rightPadding
         self.rightViewMode = UITextField.ViewMode.always
     }
@@ -46,7 +47,6 @@ public final class TextFieldComp: UITextField {
         didTapHandler?()
     }
 }
-
 
 public extension UITextField {
     func setPlaceholderColor(_ placeholderColor: UIColor) {

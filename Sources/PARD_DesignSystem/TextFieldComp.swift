27861b3cfd0504dc97b4f4de94e5f580dcaa7f0d
haram22 <kimhr9889@handong.ac.kr>
2024-03-07 15:10:56 +0900

    [Fix] delete pard textfield

--- Sources/PARD_DesignSystem/TextFieldComp.swift
+++ Sources/PARD_DesignSystem/TextFieldComp.swift
@@ -1,62 +1,64 @@
+////
+////  TextFieldComp.swift
+////
+////
+////  Created by 김하람 on 3/3/24.
+////
 //
-//  TextFieldComp.swift
+//import UIKit
 //
+//public final class TextFieldComp: UITextField {
+//    private var didTapHandler: (() -> Void)?
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//    }
+//    
+//    override public func layoutSubviews() {
+//        super.layoutSubviews()
+//    }
+//    
+//    public convenience init(
+//        placeHolder: String,
+//        didTapHandler: @escaping () -> Void
+//    ) {
+//        self.init(frame: .zero)
+//        self.backgroundColor = .pard.blackCard
+//        self.layer.cornerRadius = 4
+//        self.placeholder = placeHolder
+//        self.textColor = .white
+//        self.setPlaceholderColor(.pard.gray30)
+//        self.layer.borderColor = UIColor.pard.gray10.cgColor
+//        self.textAlignment = .left
+//        let leftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.frame.height))
+//        self.leftView = leftPadding
+//        self.leftViewMode = UITextField.ViewMode.always
 //
-//  Created by 김하람 on 3/3/24.
+//        let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width + 10, height: self.frame.height))
+//        self.rightView = rightPadding
+//        self.rightViewMode = UITextField.ViewMode.always
+//    }
+//    
+//    @objc
+//    private func didTapButton() {
+//        didTapHandler?()
+//        
+//    }
+//}
 //
-
-import UIKit
-
-public final class TextFieldComp: UITextField {
-    private var didTapHandler: (() -> Void)?
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
-    override public func layoutSubviews() {
-        super.layoutSubviews()
-    }
-    
-    public convenience init(
-        placeHolder: String
-    ) {
-        self.init(frame: .zero)
-        self.backgroundColor = .pard.blackCard
-        self.layer.cornerRadius = 4
-        self.placeholder = placeHolder
-        self.textColor = .white
-        self.setPlaceholderColor(.pard.gray30)
-        self.layer.borderColor = UIColor.pard.gray10.cgColor
-        self.textAlignment = .left
-        let leftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.frame.height))
-        self.leftView = leftPadding
-        self.leftViewMode = UITextField.ViewMode.always
-
-        let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width + 10, height: self.frame.height))
-        self.rightView = rightPadding
-        self.rightViewMode = UITextField.ViewMode.always
-    }
-    
-    @objc
-    private func didTapButton() {
-        didTapHandler?()
-    }
-}
-
-public extension UITextField {
-    func setPlaceholderColor(_ placeholderColor: UIColor) {
-        attributedPlaceholder = NSAttributedString(
-            string: placeholder ?? "",
-            attributes: [
-                .foregroundColor: placeholderColor,
-                .font: font
-            ].compactMapValues { $0 }
-        )
-    }
-    
-}
+//public extension UITextField {
+//    func setPlaceholderColor(_ placeholderColor: UIColor) {
+//        attributedPlaceholder = NSAttributedString(
+//            string: placeholder ?? "",
+//            attributes: [
+//                .foregroundColor: placeholderColor,
+//                .font: font
+//            ].compactMapValues { $0 }
+//        )
+//    }
+//    
+//}

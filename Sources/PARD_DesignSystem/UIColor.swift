d0331a7a91d63b59a4bb1cfb4157825dd50032e4
haram22 <kimhr9889@handong.ac.kr>
2024-07-04 22:12:44 +0900

    [Feat] add gradient color

--- Sources/PARD_DesignSystem/UIColor.swift
+++ Sources/PARD_DesignSystem/UIColor.swift
@@ -23,6 +23,31 @@ public extension UIColor {
         public let gray30 = UIColor(colorNamed: "gray30")!
         public let blackCard = UIColor(colorNamed: "blackCard")!
         public let blackBackground = UIColor(colorNamed: "blackBackground")!
-//        public let gra = UIColor(colorNamed: "gra")!
+        public let gra = UIColor(patternImage: gradientImage())
     }
+}
+
+func gradientLayer() -> CAGradientLayer {
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.colors = [
+        UIColor(red: 82/255, green: 98/255, blue: 245/255, alpha: 1).cgColor,
+        UIColor(red: 123/255, green: 63/255, blue: 239/255, alpha: 1).cgColor
+    ]
+    gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+    gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+    return gradientLayer
+}
+
+private func gradientImage() -> UIImage {
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+    gradientLayer.colors = [UIColor(red: 82/255, green: 98/255, blue: 245/255, alpha: 1).cgColor, UIColor(red: 123/255, green: 63/255, blue: 239/255, alpha: 1).cgColor]
+    gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+    gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+    
+    UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+    gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image!
 }
